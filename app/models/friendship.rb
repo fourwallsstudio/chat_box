373570c3b78bba3a042d8edbb786af0f3b1590c9
Calvin McElroy <fourwallsstudio@gmail.com>
2017-10-09 17:54:03 -0400

    birth chat_box

---
+++ app/models/friendship.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: friendships
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  friend_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Friendship < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :friend, class_name: :User
+end
---
+++ app/models/user.rb
@@ -0,0 +1,35 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string
+#  last_sign_in_ip        :string
+#  username               :string           not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, :email, presence: true, uniqueness: true
+
+  has_many :friendships
+  has_many :inverse_friendships, class_name: :Friendship, foreign_key: :friend_id
+
+  def friends
+    friendships + inverse_friendships
+  end
+end
